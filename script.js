55a5773e0967216880305f3b65a5aee5614f832d
amitsen12 <98091018+amitsen12@users.noreply.github.com>
2024-10-23 19:32:36 +0530

    project

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+function openpdf(){
+  
+    window.open("https://www.w3schools.com");
+}
